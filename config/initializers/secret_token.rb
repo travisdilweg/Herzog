fbdd0de7c4c52e3f0f87841c29b8ebe895e19af8
Travis Dilweg <you@example.com>
2015-09-01 14:39:39 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Herzog::Application.config.secret_key_base = 'f18218a063c0dcbaf345543da1e88e56f739680bd8738a3a60d662db8bf0f34e1f0d176be48df01ce250fa8ce7a932a35c1581d190a89209d2ddaa074e2a5c3c'
